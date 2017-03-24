92aba9f654ca86f05f9025e5e18acbb021ceeaef
patykov <patriciakovaleski@gmail.com>
2017-03-24 00:40:47 -0300

    Mais metricas

--- Trabalho1/graphStatistics.py
+++ Trabalho1/graphStatistics.py
@@ -12,14 +12,6 @@ if __name__ == '__main__':
 
 	edges = g.get_edges()
 
-	# vb, eb = betweenness(g)
-	# graph_draw(g, pos=g.vb["pos"], vertex_fill_color=vb, vertex_size=graph_tool.prop_to_size(vb, mi=5, ma=15),
-	# 	            edge_pen_width=graph_tool.prop_to_size(eb, mi=0.5, ma=5), vcmap=matplotlib.cm.gist_heat,
-	# 	            vorder=vb, output="betweenness.pdf")
-
-	# hist = graph_tool.stats.distance_histogram(g, samples=20)
-	# print(hist)
-
 	# Grau Medio
 	grau_medio, dp_grau_medio = graph_tool.stats.vertex_average(g, 'total')
 	print(grau_medio, dp_grau_medio)
@@ -28,6 +20,10 @@ if __name__ == '__main__':
 	dist_map = graph_tool.topology.shortest_distance(g)
 	dist, dp_dist = graph_tool.stats.vertex_average(g, dist_map)
 	shortest_dist = np.amin(dist)
-	dp_shortest_dist = [dp_dist[i] for (i,x) in enumerate(dist) if x==shortest_dist]
+	dp_shortest_dist = [dp_dist[i] for (i,x) in enumerate(dist) if x==shortest_dist][0]
 	print(shortest_dist, dp_shortest_dist)
 
+	# Betwenness
+	vb_map, eb_map = graph_tool.centrality.betweenness(g)
+	vb, dp_vb = graph_tool.stats.vertex_average(g, vb_map)
+	print(vb, dp_vb)
